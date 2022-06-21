0c432b7cfa7bc3bbd8093364eea836f6bd37fbb6
Scott Scherzer <70346066+SlideeScherz@users.noreply.github.com>
2022-06-21 14:17:43 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const Nightmare = require("nightmare");
+const nightmare = Nightmare({ show: true });
+
+nightmare
+  .goto("https://www.hackerrank.com/auth/login")
+  .type("#input-1", "")
+  .type("#input-2", "")
+  .click(
+    "#tab-1-content-1 > div.login-form.auth-form.theme-m > form > div.form-item.clearfix > button"
+  )
+  .wait()
+  .end()
+  .then(console.log)
+  .catch((error) => {
+    console.error("Search failed:", error);
+  });
+
+//wait for submissions button
+
+//go there
+
+//get all accepted answers
+
+//make array for each
+
+//keep the most recent, passing/highest scores
+
+//copy each URL and write to file with correct extension
+
+//push to github
